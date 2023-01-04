2115f869ba5ca9ec490754bb029f100634ca18ae
김민성 <sohoung4@gmail.com>
2023-01-04 16:18:41 +0900

    홀 수 더하기

--- SW-expert-Academy/src/D1/홀수더하기.java
+++ SW-expert-Academy/src/D1/홀수더하기.java
@@ -1,4 +1,21 @@
 package D1;
 
+// 설명 : 10개의 수를 입력받아 그 중에서 홀수만 더하는 값을 출력하는 프로그램을 작성하세요.
+// 입력 예시 : 3
+//           3 17 1 39 8 41 2 32 99 2
+//           22 8 5 123 7 2 63 7 3 46
+//           6 63 2 3 58 76 21 33 8 1
+// 출력 예시 : #1 200
+//           #2 208
+//           #3 121
+
 public class 홀수더하기 {
+    public int solution(int n, int[] arr) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        
+    }
 }
