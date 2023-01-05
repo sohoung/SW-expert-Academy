55e7291c79198016d5b825f34b802a09092089d8
김민성 <sohoung4@gmail.com>
2023-01-05 17:36:05 +0900

    홀 수 더하기

--- SW-expert-Academy/src/D1/홀수더하기.java
+++ SW-expert-Academy/src/D1/홀수더하기.java
@@ -13,21 +13,23 @@ import java.util.Scanner;
 
 public class 홀수더하기 {
     public static void main(String[] args) {
-        홀수더하기  m = new 홀수더하기();
+        홀수더하기 m = new 홀수더하기();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();  // test 갯수
-        int[] sumarr = new int[n];  // sum 값을 저장하는 배열
-        int[][] arr = new int[n][10];
-        for(int j = 0; j < n; j++) {
-            int sum = 0;
-            for (int i = 0; i < arr.length; i++) {
-                arr[j][i] = sc.nextInt();
-            }
-            sumarr[j] = sum;
-        }
-
+        int[][] arr = new int[n][10];  // 입력받은 10개의 수 3번을 저장하는 2차원 배열
+        int[] sumarr = new int[n];  // 합을 저장하는 합 배열
         for(int i = 0; i < n; i++) {
-            System.out.println("#" + i + " " + sumarr[i]);
+            int sum = 0;  // 다음 sum을 위해서 sum을 0으로 초기화
+            for(int j = 0; j < 10; j++) {
+                arr[i][j] = sc.nextInt();  // 2중 for문으로 2차원 배열 값 저장
+                if(arr[i][j] % 2 == 1) {  // if 조건문으로 arr 배열에 있는 데이터 값이 2로 나누었을 때 나머지가 1인 경우 즉 홀수인 경우에
+                    sum += arr[i][j];     // sum에 arr의 배열 값을 누적합해주고
+                    sumarr[i] = sum;      // 출력을 위해서 sumarr 배열에 sum 값을 넣어준다.
+                }
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            System.out.println("#" + (i+1) + " " + sumarr[i]);
         }
     }
 }
