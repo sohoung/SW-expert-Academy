c6d6aec16395016ed3ff94fee9df23cc18a7a1a1
김민성 <sohoung4@gmail.com>
2023-01-09 14:02:40 +0900

    놈놈놈

--- SW-expert-Academy/src/D1/놈놈놈.java
+++ SW-expert-Academy/src/D1/놈놈놈.java
@@ -19,13 +19,13 @@ public class 놈놈놈 {
             int a = sc.nextInt();  // 비교할 두 개의 수를 입력
             int b = sc.nextInt();  // 비교할 두 개의 수를 입력
             if(a - b == 0) {  // if - else if 문으로 조건에 따른 다른 출력값 char 배열에 저장
-                ch[i] = '=';
+                ch[i] = '=';  //  a에서 b를 뺀 값이 0이면 a와 b가 같으므로 '='
             }
-            else if(a - b > 0){
+            else if(a - b > 0){  // a에서 b를 뺀 값이 양수이면 a가 더 크다는 의미이므로 '>'
                 ch[i] = '>';
             }
             else {
-                ch[i] = '<';
+                ch[i] = '<';  // a에서 b를 뺀 값이 음수이면 b가 a보다 더 큰 값이므로 '<'
             }
         }
         for(int i = 0; i < n; i++) {
