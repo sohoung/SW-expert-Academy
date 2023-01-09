327e42746e4b2cc65af0068839262c4eeb321fa0
김민성 <sohoung4@gmail.com>
2023-01-09 16:14:36 +0900

    연월일 달력

--- SW-expert-Academy/src/D1/연월일달력.java
+++ SW-expert-Academy/src/D1/연월일달력.java
@@ -17,30 +17,38 @@ import java.util.Scanner;
 //           #5 1111/11/11
 public class 연월일달력 {
     static int[] daycount = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    // 1월부터 12월까지의 마지막 일수를 저장하는 배열이다. 인덱스 0번은 그냥 없는 것으로 생각하고 1번 인덱스를 1월로 생각한다.
 
     public static void main(String[] args) {
         연월일달력 m = new 연월일달력();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+        int n = sc.nextInt();  // 입력 갯수
+        for (int i = 1; i <= n; i++) {  // 인덱스를 1번부터 사용하므로 1부터 시작
             String str = sc.next();
-            int year = Integer.parseInt(str.substring(0, 4));
-            int month = Integer.parseInt(str.substring(4, 6));
-            int day = Integer.parseInt(str.substring(6, 8));
+            int year = Integer.parseInt(str.substring(0, 4));  // substring으로 0번 문자열부터 3번 문자열까지 추출
+            int month = Integer.parseInt(str.substring(4, 6)); // substring으로 4번 문자열부터 5번 문자열까지 추출
+            int day = Integer.parseInt(str.substring(6, 8));   // substring으로 6번 문자열부터 7번 문자열까지 추출
 
             if (month > 0 && month < 13 && day <= daycount[month]) {
-                if (year < 1000) {
+                // month는 달로 1월부터 12월까지 입력이되고 day는 daycount에 저장되어있는 일수와 같거나 작다면 true
+                if (year < 1000) {  // 년수가 1000년 이하라면 앞에 0을 붙여야 하므로 따로 if문 생성
                     if (month < 10 && day < 10) {
+                        // month가 10이하이면 앞에 0, day가 10보다 작으면 앞에 0
                         System.out.println("#" + i + " 0" + year + "/0" + month + "/0" + day);
                     } else if (month < 10) {
+                        // month만 10보다 작으면 앞에 0
                         System.out.println("#" + i + " 0" + year + "/0" + month + "/" + day);
                     }
-                } else if (month < 10) {
+                }
+                else if (month < 10) {
+                    // 1000년 이상일 경우에서 month가 10 이하인경우
                     System.out.println("#" + i + " " + year + "/0" + month + "/" + day);
                 } else {
+                    // 1000년 이상이고 month가 10이상이고 day가 10이상
                     System.out.println("#" + i + " " + year + "/" + month + "/" + day);
                 }
             } else {
+                // 말이 안되는 달력일 때
                 System.out.println("#" + i + " " + -1);
             }
         }
