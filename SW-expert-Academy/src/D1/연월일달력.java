5ab5155ebd4f4a8171e4efa872ca8458cb049147
김민성 <sohoung4@gmail.com>
2023-01-09 15:54:11 +0900

    연월일 달력

--- SW-expert-Academy/src/D1/연월일달력.java
+++ SW-expert-Academy/src/D1/연월일달력.java
@@ -1,4 +1,5 @@
 package D1;
+
 import java.util.Scanner;
 
 // 설명 : 연월일 순으로 구성된 8자리의 날짜가 주어진다. 해당 날짜의 유효성을 판단한 후 날짜가 유효하다면 "YYYY/MM/DD" 형식으로 출력하고
@@ -15,19 +16,33 @@ import java.util.Scanner;
 //           #4 -1
 //           #5 1111/11/11
 public class 연월일달력 {
-    static int[] daycount = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    static int[] daycount = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     public static void main(String[] args) {
-        연월일달력  m = new 연월일달력();
+        연월일달력 m = new 연월일달력();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++) {
-            Stirng str = sc.next();
-            int year = Integer.parseInt(str.substring(0,4));
-            int month = Integer.parseInt(str.substring(4,6));
-            int day = Integer.parseInt(str.substring(6,8));
-        }
-        for(int i = 0 ; i < n; i++) {
-            System.out.println("#" + (i+1) + " " + year + "/" + month + "/" + day);
+        for (int i = 1; i <= n; i++) {
+            String str = sc.next();
+            int year = Integer.parseInt(str.substring(0, 4));
+            int month = Integer.parseInt(str.substring(4, 6));
+            int day = Integer.parseInt(str.substring(6, 8));
+
+            if (month > 0 && month < 13 && day <= daycount[month]) {
+                if (year < 1000) {
+                    if (month < 10 && day < 10) {
+                        System.out.println("#" + i + " 0" + year + "/0" + month + "/0" + day);
+                    } else if (month < 10) {
+                        System.out.println("#" + i + " 0" + year + "/0" + month + "/" + day);
+                    }
+                } else if (month < 10) {
+                    System.out.println("#" + i + " " + year + "/0" + month + "/" + day);
+                } else {
+                    System.out.println("#" + i + " " + year + "/" + month + "/" + day);
+                }
+            } else {
+                System.out.println("#" + i + " " + -1);
+            }
         }
     }
 }
