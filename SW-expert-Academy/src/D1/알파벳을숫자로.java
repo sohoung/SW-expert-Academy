0f1251e27d270f3c2564c37d5f4acc7a6a208f5a
김민성 <sohoung4@gmail.com>
2023-01-10 11:24:03 +0900

    알파벳을 숫자로

---
+++ SW-expert-Academy/src/D1/알파벳을숫자로.java
@@ -0,0 +1,15 @@
+package D1;
+import java.util.Scanner;
+
+// 설명 : 알파벳으로 이루어진 문자를 입력받 각 알파벳을 1부터 26까지의 숫자로 변환하여 출력하라
+// 입력 예시 : ABCDEFGHIJKLMNOPQRSTUVWXYZ
+// 출력 예시 : 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+
+public class 알파벳을숫자로 {
+    public static void main(String[] args) {
+        알파벳을숫자로  m = new 알파벳을숫자로();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        
+    }
+}
