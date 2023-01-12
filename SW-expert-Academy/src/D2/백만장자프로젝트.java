62a120a425380319366d852911a80b4fb375ea66
김민성 <sohoung4@gmail.com>
2023-01-12 22:22:19 +0900

    백만 장자 프로젝트

---
+++ SW-expert-Academy/src/D2/백만장자프로젝트.java
@@ -0,0 +1,11 @@
+package D2;
+import java.util.Scanner;
+
+// 설명 :
+public class 백만장자프로젝트 {
+    public static void main(String[] args) {
+        백만장자프로젝트  m = new 백만장자프로젝트();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
