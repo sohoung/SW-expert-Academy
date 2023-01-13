54c909465ff3b779a2427c7de8284f3eb4be9efe
김민성 <sohoung4@gmail.com>
2023-01-13 22:07:42 +0900

    간단한 369 게임

--- SW-expert-Academy/src/D2/Game369.java
+++ SW-expert-Academy/src/D2/Game369.java
@@ -1,4 +1,5 @@
 package D2;
+
 import java.util.Scanner;
 
 // 설명 : 3 6 9 게임을 프로그램으로 제작중이다. 게임 규칙은 다음과 같다.
@@ -16,5 +17,32 @@ public class Game369 {
     public static void main(String[] args) {
         Game369 m = new Game369();
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;  // 검증을 위한 cnt 변수
+            if (i < 10) {  // 10 이하의 수
+                if (i % 3 == 0) {  // 3으로 나눈 나머지가 0이면 3 6 9중에 하나
+                    System.out.print("- ");
+                } else {
+                    System.out.print(i + " ");
+                }
+            } else {
+                String str = Integer.toString(i);  // 2자리 숫자부터는 배열을 사용해서 각각 검사
+                char[] ch = str.toCharArray();  // str을 기반으로 char형 배열 ch 생성
+                for (int j = 0; j < ch.length; j++) {  // for문으로 배열 안에 있는 데이터를 각각 검증
+                    if ((ch[j] - '0') != 0 && (ch[j] - '0' ) % 3 == 0) {  
+                        cnt++;
+                    }
+                }
+                if (cnt == 0) {  // cnt가 0이라는 말은 숫자에 3, 6, 9가 하나도 안들어가 있다는 말이다.
+                    System.out.print(i + " ");
+                } else {  // cnt가 > 0 이라면 숫자에 3,6,9,가 적어도 1개는 들어있다는 말이다.
+                    for (int k = 0; k < cnt; k++) {
+                        System.out.print("-");  // 3,6,9의 숫자만큼 "-"을 출력해준다.
+                    }
+                    System.out.print(" ");
+                }
+            }
+        }
     }
 }
