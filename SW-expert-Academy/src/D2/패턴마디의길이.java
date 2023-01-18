10ea4ddfd2c88dfe4452c5d3cc92e776875b572b
김민성 <sohoung4@gmail.com>
2023-01-18 13:44:04 +0900

    패턴 마디의 길이

--- SW-expert-Academy/src/D2/패턴마디의길이.java
+++ SW-expert-Academy/src/D2/패턴마디의길이.java
@@ -1,4 +1,20 @@
 package D2;
 
+import java.util.Scanner;
+
+// 설명 : 패턴에서 반복되는 부분을 마디라고 부른다. 문자열을 입력 받아 마디의 길이를 출력하는 프로그램을 작성하라.
+// 입력 에시 : 3
+//           KOREAKOREAKOREAKOREAKOREAKOREA
+//           SAMSUNGSAMSUNGSAMSUNGSAMSUNGSA
+//           GALAXYGALAXYGALAXYGALAXYGALAXY
+// 출력 에시 : #1 5
+//           #2 7
+//           #3 6
 public class 패턴마디의길이 {
+    public static void main(String[] args) {
+        패턴마디의길이 m = new 패턴마디의길이();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  // 테스트 갯수
+        
+    }
 }
